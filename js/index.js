f574d83f2d1db032bb3eef3cf449edd823c01e90
Salaah Amin <48691708+Salaah01@users.noreply.github.com>
2019-04-05 07:57:39 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,13 +1,45 @@
+// When the menu button is pressed
 $("#open-address").on("click", function() {
-    $(".address").removeClass("hide");
+    $(".address").css("opacity", "1");
+    $(".address").css("width", $(".top-container").width());
+    $(".address").css("height", $(".top-container").height()+20);
+    
+    $(".address").addClass("open-address");
 })
 
+// When the addess is completed and submitted
 $("#submit-address").on("click", function() {
     var postcode = $("#postcode").val();
     postcode = postcode.replace(" ","");
     getLocationAPI(postcode);
-    
 });
+
+// When the window resizes, change the size of the address div.
+$(window).on("resize", function() {
+    $(".address").css("width", $(".top-container").width());
+    $(".address").css("height", $(".top-container").height()+20);
+});
+
+$("input").on("click", function() {
+    checkAddressFields();
+});
+
+$("input").on("change", function() {
+    checkAddressFields();
+});
+
+// If Address Line 1 and Post Code are completed, then it will show the complete button, otherwise it will show the incomplete button.
+function checkAddressFields() {
+    if ($("#address-line-1").val() === "" || $("#postcode").val() === "") {
+        $("#submit-address").val("Incomplete");
+        $("#submit-address").removeClass("green-btn");
+        $("#submit-address").addClass("red-btn");
+    } else {
+        $("#submit-address").val("Continue");
+        $("#submit-address").addClass("green-btn");
+        $("#submit-address").removeClass("red-btn");
+    }
+};
 
 
 function getLocationAPI(postcode) {
